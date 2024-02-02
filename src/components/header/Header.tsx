ae0fc033032b210ece9faa334b41889a2e599f3d
gibran <haikalgakbar@gmail.com>
2024-02-02 14:11:23 +0700

    refactor: header ui

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import HeaderAction from "./HeaderAction";
 import OptionMenu from "../OptionMenu";
-import { Check } from "@phosphor-icons/react";
+import {
+  SlidersHorizontal,
+  MagnifyingGlass,
+  NotePencil,
+  Check,
+  SquaresFour,
+} from "@phosphor-icons/react";
 import { DISPLAY_KEY } from "../../utils/const";
 
 export default function Header({
@@ -14,8 +20,10 @@ export default function Header({
 }) {
   const [openSortMenu, setOpenSortMenu] = useState(false);
   const [openViewMenu, setOpenViewMenu] = useState(false);
+  const [openDisplayMenu, setOpenDisplayMenu] = useState(false);
   const sortRef = useRef(null);
   const viewRef = useRef(null);
+  const displayRef = useRef(null);
   const countNotes = allNotes.length;
 
   useEffect(() => {
@@ -46,6 +54,20 @@ export default function Header({
     };
   }, [openSortMenu]);
 
+  useEffect(() => {
+    const closeMenu = (e) => {
+      if (!displayRef.current?.contains(e.target)) {
+        setOpenDisplayMenu(false);
+      }
+    };
+
+    document.addEventListener("mouseup", closeMenu);
+
+    return () => {
+      document.removeEventListener("mouseup", closeMenu);
+    };
+  }, [openDisplayMenu]);
+
   function changeView(type: "row" | "grid"): void {
     if (display.view !== type) {
       setDisplay({ ...display, view: type });
@@ -73,8 +95,103 @@ export default function Header({
   }
 
   return (
-    <header className="sticky z-10 bg-background-base-3-light dark:bg-background-base-3-dark">
-      <section className="m-2 mt-0 flex items-center justify-between rounded-xl bg-background-base-1-light p-4 dark:bg-background-base-1-dark">
+    <header className="sticky z-10 flex flex-col gap-4 rounded-t-xl bg-background-base-1-light p-4 dark:bg-background-base-1-dark">
+      <section className="flex items-center gap-2 rounded-full bg-background-base-3-dark px-4 py-2 focus:outline focus:outline-slate-400 focus:ring-1">
+        <MagnifyingGlass className=" text-foreground-base-3-dark" size={20} />
+        <input
+          className="flex-1 border-none bg-transparent text-foreground-base-1-light outline-none dark:text-foreground-base-1-dark"
+          type="text"
+          placeholder="Search notes"
+        />
+      </section>
+      <section className="flex items-center justify-between">
+        <button onClick={() => setOpenDisplayMenu(true)}>
+          <SlidersHorizontal
+            className="m-2 text-foreground-base-1-light dark:text-foreground-base-1-dark"
+            size={24}
+          />
+        </button>
+        <h1 className="flex-1 text-center text-xl font-semibold text-foreground-base-1-light dark:text-foreground-base-1-dark">
+          {countNotes}
+          {countNotes < 1 ? ` Note` : ` Notes`}
+        </h1>
+        <button onClick={() => openNewNotes(true)}>
+          <NotePencil
+            className="m-2 text-foreground-base-1-light dark:text-foreground-base-1-dark"
+            size={24}
+          />
+        </button>
+      </section>
+      {openDisplayMenu && (
+        <OptionMenu
+          getRef={displayRef}
+          className={
+            "absolute bottom-16 left-4 flex min-w-36 flex-col rounded-md border-[.5px] border-stroke-base-3-light bg-background-base-1-light p-1 shadow-md dark:border-stroke-base-3-dark dark:bg-background-base-1-dark"
+          }
+        >
+          <p className="right px-2 py-1 text-sm text-foreground-base-3-light dark:text-foreground-base-3-dark">
+            Sort
+          </p>
+          <button
+            className="flex items-center justify-between gap-1 rounded-md p-2 text-sm text-foreground-base-1-light hover:bg-background-hover-1-light dark:text-foreground-base-1-dark dark:hover:bg-background-hover-1-dark"
+            onClick={() => changeView("row")}
+          >
+            A to Z
+            {display.view === "row" && (
+              <Check className="flex-shrink-0" size={20} />
+            )}
+          </button>
+          <button
+            className="flex items-center justify-between gap-1 rounded-md p-2 text-sm text-foreground-base-1-light hover:bg-background-hover-1-light dark:text-foreground-base-1-dark dark:hover:bg-background-hover-1-dark"
+            onClick={() => changeView("grid")}
+          >
+            Z to A
+            {display.view === "grid" && (
+              <Check className="flex-shrink-0" size={20} />
+            )}
+          </button>
+          <p className="right px-2 py-1 text-sm text-foreground-base-3-light dark:text-foreground-base-3-dark">
+            View
+          </p>
+          <button
+            className="flex items-center justify-between gap-1 rounded-md p-2 text-sm text-foreground-base-1-light hover:bg-background-hover-1-light dark:text-foreground-base-1-dark dark:hover:bg-background-hover-1-dark"
+            onClick={() => changeView("row")}
+          >
+            Row
+            {display.view === "row" && (
+              <Check className="flex-shrink-0" size={20} />
+            )}
+          </button>
+          <button className="flex flex-wrap items-center gap-1 rounded-md p-2 text-sm text-foreground-base-1-light hover:bg-background-hover-1-light dark:text-foreground-base-1-dark dark:hover:bg-background-hover-1-dark">
+            <SquaresFour size={20} />
+            <p className="flex-1 text-start">Grid</p>
+            <div className="flex items-center">
+              <div className="relative flex h-4 w-4 flex-shrink-0 items-center justify-center rounded-full bg-white dark:bg-gray-100">
+                <input
+                  aria-labelledby="label1"
+                  checked
+                  type="radio"
+                  name="radio"
+                  className="absolute h-full w-full cursor-pointer appearance-none rounded-full border border-gray-400 checked:border-none focus:opacity-100 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-offset-2"
+                />
+                <div className="z-1 hidden h-full w-full rounded-full border-4 border-indigo-700"></div>
+              </div>
+              <label
+                id="label1"
+                className="ml-2 text-sm font-normal leading-4 text-gray-800 dark:text-gray-100"
+              >
+                On
+              </label>
+            </div>
+          </button>
+        </OptionMenu>
+      )}
+    </header>
+  );
+}
+
+{
+  /* <section className="flex items-center justify-between p-4">
         <h1 className="text-xl font-semibold text-foreground-base-1-light dark:text-foreground-base-1-dark">
           {countNotes}
           {countNotes < 1 ? ` Note` : ` Notes`}
@@ -149,7 +266,5 @@ export default function Header({
             </button>
           </OptionMenu>
         )}
-      </section>
-    </header>
-  );
+      </section> */
 }
